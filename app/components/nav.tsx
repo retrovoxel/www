a951c1cb112edb619f5b30b34090de58fbba635c
retrovoxel <retrovoxel@protonmail.com>
2023-10-02 01:35:11 +0530

    init: basic website

---
+++ app/components/nav.tsx
@@ -0,0 +1,7 @@
+export default function Nav () {
+    return (
+        <div className=" hidden:border hidden:border-1 pt-10 pb-5 flex justify-start items-center">
+        <a href="/" className=" text-lg text-stone-400 font-medium">retrovoxel</a>
+      </div>
+    )
+}
