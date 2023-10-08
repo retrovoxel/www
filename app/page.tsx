291ef4617285eec99d03b6cd3e1c0577c1eefe89
retrovoxel <retrovoxel@protonmail.com>
2023-10-09 00:03:43 +0530

    refactor: data obj

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,46 @@
 import Nav from "./components/nav";
 import { Github, Twitter, Mail } from "lucide-react";
+
+interface Project {
+  title: string;
+  desc: string;
+}
+
+interface Link {
+  title: string;
+  link: string;
+}
+
+const projects: Project[] = [
+  {
+    title: "colabcoder",
+    desc: "write code together with friends in real time",
+  },
+  {
+    title: "moe-cli",
+    desc: "command line tool to search, download and read manga",
+  },
+  {
+    title: "wisepay-backend",
+    desc: "rest api for an expense manager with jwt authentication and crud operations",
+  },
+];
+
+const links: Link[] = [
+  {
+    title: "Github",
+    link: "https://www.github.com/retrovoxel",
+  },
+  {
+    title: "Twitter",
+    link: "https://www.twitter.com/retrovoxel",
+  },
+  {
+    title: "Email",
+    link: "mailto:retrovoxel@protonmail.com",
+  },
+];
+
 export default function Home() {
   return (
     <main className="bg-[rgb(23,23,23)] flex justify-center min-h-screen w-full">
@@ -10,45 +51,27 @@ export default function Home() {
           projects
         </p>
         <ul className=" pb-10 flex flex-col gap-5">
-          <li>
-            <a
-              href="https://www.github.com/retrovoxel/moe-cli"
-              className="pb-2 text-sm md:text-base text-white font-normal underline underline-offset-4"
-            >
-              moe-cli
-            </a>
-            <p className="text-sm md:text-base text-stone-400 font-normal ">
-              command line tool to search, download and read manga
-            </p>
-          </li>
-          <li>
-            <a
-              href="https://www.github.com/retrovoxel/wisepay-backend"
-              className=" pb-2 text-sm md:text-base text-white font-normal underline underline-offset-4"
-            >
-              wisepay
-            </a>
-            <p className=" text-sm md:text-base text-stone-400 font-normal ">
-              rest api for an expense manager with jwt authentication and crud operations
-            </p>
-          </li>
-          <li>
-            <a
-              href="https://www.github.com/retrovoxel/colabcoder"
-              className="pb-2 text-sm md:text-base text-white font-normal underline underline-offset-4"
-            >
-              colabcoder
-            </a>
-            <p className="text-sm md:text-base text-stone-400 font-normal ">
-              write code together with friends in real time
-            </p>
-          </li>
+          {projects.map((project, index) => (
+            <li key={index}>
+              <a
+                href={`https://www.github.com/retrovoxel/${project.title}`}
+                className="pb-2 text-sm md:text-base text-white font-normal underline underline-offset-4"
+              >
+                {project.title}
+              </a>
+              <p className="text-sm md:text-base text-stone-400 font-normal ">
+                {project.desc}
+              </p>
+            </li>
+          ))}
         </ul>
         <hr />
+
         <div className=" py-7 flex gap-5">
           <a href="https://www.github.com/retrovoxel">
             <Github color="white" />
           </a>
+
           <a href="https://www.twitter.com/retrovoxel">
             <Twitter color="white" />
           </a>
